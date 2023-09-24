95948904fd81e7f969ca57c1cc3b0c3d93d44d14
l_jiujiu <LIU_jiujiu0124@163.com>
2023-09-24 11:35:32 +0800

    20230924 trim the doc

---
+++ Storage/DataPreparation.py
@@ -0,0 +1,255 @@
+import numpy as np
+import pandas as pd
+import os
+import openpyxl
+
+from a_Class import Sku
+
+def init_sku_time(path_sku_time_map,num_sku,Mean,StandardDeviation):
+    # 生成time随机数
+    random_normal = np.random.normal(Mean, StandardDeviation, num_sku)
+    sku_time = []
+    for rdm in random_normal:
+        if (rdm < 1):
+            rdm = 1
+        sku_time.append(round(rdm, 0))
+
+    wb_file=path_sku_time_map
+    data_time = pd.DataFrame(sku_time)
+    data_time.columns = ['Time']
+    book = openpyxl.load_workbook(wb_file)  # 读取你要写入的workbook
+    writer = pd.ExcelWriter(wb_file, engine='openpyxl')
+    writer.book = book
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+    data_time.to_excel(writer, sheet_name="Part 1", index=False, startcol=4, startrow=0)
+    writer.save()
+
+def init_sku_time_1(path_sku_time_map,num_sku):
+    # 生成time随机数
+    sku_time=[1]*num_sku
+
+    wb_file=path_sku_time_map
+    data_time = pd.DataFrame(sku_time)
+    data_time.columns = ['Time']
+    book = openpyxl.load_workbook(wb_file)  # 读取你要写入的workbook
+    writer = pd.ExcelWriter(wb_file, engine='openpyxl')
+    writer.book = book
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+    data_time.to_excel(writer, sheet_name="Part 1", index=False, startcol=4, startrow=0)
+    writer.save()
+
+def evaluation(vars,flag):
+    cwd = os.getcwd()  # 读取当前文件夹路径
+
+    # path_order_sku_map = cwd + '/Fa_data/backup/OrderPickDetail.xlsx'
+    # path_sku_time_map = cwd + '/Fa_data/backup/PickLinePos_time(235,有null).xlsx'
+
+    path_order_sku_map = cwd + '/Fa_data/OrderPickDetail.xlsx'
+    path_sku_time_map = cwd + '/Fa_data/PickLinePos_time.xlsx'
+
+    # 初始化sku所在的分区：sku名称，sku处理所需时间、sku所在分区
+    df = pd.read_excel(path_sku_time_map, sheet_name='Part 1', usecols='B,C,E')
+    df.dropna(axis=0, how='any', inplace=True)
+    df.reset_index(inplace=True,drop=True)
+    data_count = df.values
+    num_sku = len(data_count)
+    print('所有Sku数量为：%d ' % num_sku)
+    # print(vars)
+    # print(df)
+
+    # 重新读取更新后的SKU Time数据
+    if(flag==1):
+        df['Time']=pd.Series(vars)
+
+    sku_list=[]
+    data = df.values
+    for i in range(0, num_sku):
+        sku_input = {
+            'name': str(int(data[i][1])),  # sku名称
+            'num': i,  # sku序号
+            'sectionID': str(int(data[i][0])),  # GroupID,sku所在分区
+            'sku_time': vars[i],  # sku处理所需时间（默认为1）
+
+            # 'sku_time': vars[i],  # sku处理所需时间（默认为1）
+        }
+        sku_list.append(Sku(sku_input))
+    # ————————————————————————————
+    # 初始化order，sku表dataframe叫df，order表dataframe叫data
+    data = pd.read_excel(path_order_sku_map, sheet_name='Part 1', usecols='A,B,C,D',
+                         names=['OrderID', 'CommodityID', 'Amount', 'PosGroupID'])
+    # data[data['CommodityID']==329725618]['PosGroupID']=2101
+
+    # 根据SKU的处理用时和订单包含的SKU个数计算单SKU的处理时间
+    data_skuorder = pd.merge(data, df, on=['CommodityID', 'PosGroupID'], how='left')
+    # data_skuorder.dropna(axis=0, how='any', inplace=True)
+    print(data_skuorder[data_skuorder.isna().any(axis=1)])
+
+    data_skuorder.insert(data_skuorder.shape[1], 'Time*Amount',
+                         data_skuorder['Amount'] * data_skuorder['Time'])
+    # data_num = data_skuorder['PosGroupID'].count()  # order拆散的sku信息总条数
+    num_order = data_skuorder['PosGroupID'].groupby(data_skuorder['OrderID']).count().size  # 统计订单总个数
+
+    # print(self.num_order)
+    # print(data_skuorder)
+    data_order_gb = data_skuorder.groupby(['OrderID', 'PosGroupID'])['Time*Amount'].sum()  # 统计order在各section的用时之和
+    data_order = pd.DataFrame(data_order_gb)
+    data_order.reset_index(inplace=True)
+    data_order['id'] = data_order['OrderID'].rank(ascending=1, method='dense').astype(int)
+    # print(data_order)
+
+    order_array = np.zeros((num_order, 6))
+    order_array_01 = np.zeros((num_order, 6))
+
+    # 创建初始order-section矩阵[行数line:num_order,列数col:num_section[8个],按照012345，-1，-2]
+    for index, row in data_order.iterrows():
+        # print(index)
+        # 修改订单用时矩阵
+        order_array[int(row['id'] - 1), int(row['PosGroupID'] / 100 - 17)] = row['Time*Amount']
+        order_array_01[int(row['id'] - 1), int(row['PosGroupID'] / 100 - 17)] = 1
+
+    # np.set_printoptions(threshold=np.sys.maxsize)
+    # print(f'order_array 6:{self.order_array}')
+    order_mainstream_workstep = np.ones((1, num_order))
+    order_array = np.insert(order_array, 6, order_mainstream_workstep, axis=1)
+    order_array = np.insert(order_array, 7, order_mainstream_workstep, axis=1)
+    order_array_01 = np.insert(order_array_01, 6, np.zeros((1, num_order)), axis=1)
+    order_array_01 = np.insert(order_array_01, 7, np.zeros((1, num_order)), axis=1)
+
+    # print(
+    #     f'所有SKU数量为:{num_sku},所有Order数量为:{num_order}')
+
+    total_workload_array=order_array.sum(axis=0)
+    total_workload_array01=order_array_01.sum(axis=0)
+
+    print(list(order_array.sum(axis=0)))
+    print(list(order_array_01.sum(axis=0)))
+    # print(len(order_array.sum(axis=0)))
+    # print(order_array.sum(axis=0))
+
+    busy_sad=0
+    for i in range(0,6):
+        for j in range(i+1,6):
+            buff=abs(total_workload_array[i] - total_workload_array[j])
+            busy_sad = busy_sad + buff
+            # print(f'{i}-{j}:{buff}')
+    # print(busy_sad)
+    # print('\n')
+
+    busy_sad01 = 0
+    for i in range(0, 6):
+        for j in range(i + 1, 6):
+            buf=abs(total_workload_array01[i] - total_workload_array01[j])
+            busy_sad01 = busy_sad01 + buf
+            # print(f'{i}-{j}:{buf}')
+    # print(busy_sad01)
+    return busy_sad
+
+def test_sku_section_popularity():
+    # 用于测试各[section,sku]的popularity
+    cwd = os.getcwd()  # 读取当前文件夹路径
+
+    path_order_sku_map = cwd + '/Fa_data/OrderPickDetail.xlsx'
+    path_sku_time_map = cwd + '/Fa_data/PickLinePos_time.xlsx'
+
+    data_order = pd.read_excel(path_order_sku_map, sheet_name='Part 1', usecols='A,B,C,D',
+                         names=['OrderID', 'CommodityID', 'Amount', 'PosGroupID'])
+    print('-------------')
+    # print(data_order.groupby('PosGroupID')['OrderID'].apply(lambda x:len(x)))
+    # print(data_order.groupby('CommodityID')['OrderID'].apply(lambda x:len(x)))
+
+    df=data_order.groupby(['PosGroupID','CommodityID'])['OrderID'].apply(lambda x: len(x))
+    data_order = pd.DataFrame(df)
+    data_order.reset_index(inplace=True, drop=False)
+    data_order=data_order.sort_values(by='OrderID',ascending=False)
+    print(data_order.head(10))
+    # print(data_order.groupby('PosGroupID')['OrderID'].apply(lambda x:len(x)))
+
+
+def test_sku_amount_popularity():
+    cwd = os.getcwd()  # 读取当前文件夹路径
+
+    path_order_sku_map = cwd + '/Fa_data/OrderPickDetail.xlsx'
+
+    data_order = pd.read_excel(path_order_sku_map, sheet_name='Part 1', usecols='A,B,C,D',
+                               names=['OrderID', 'CommodityID', 'Amount', 'PosGroupID'])
+    df=data_order.groupby(['CommodityID','PosGroupID'])['Amount'].apply(lambda x:sum(x))
+    data_order = pd.DataFrame(df)
+    print(data_order)
+    data_order.reset_index(inplace=True, drop=False)
+    data_order = data_order.sort_values(by='Amount', ascending=False)
+    print(data_order.head(10))
+    # print(data_order)
+    dff=data_order.groupby('PosGroupID').apply(lambda x:x.sort_values('Amount',ascending=False))
+    print(dff)
+    # dff.to_excel('excel_1.xlsx',sheet_name='Sheet1',index=True)
+
+def sku_all_used():
+    # 用于测试sku表中的235个sku是否都被order list用到了
+    cwd = os.getcwd()  # 读取当前文件夹路径
+    path_order_sku_map = cwd + '/Fa_data/OrderPickDetail.xlsx'
+    path_sku_time_map = cwd + '/Fa_data/PickLinePos_time.xlsx'
+
+    # data_sku = pd.read_excel(path_sku_time_map, sheet_name='Part 1', usecols='B,C,E')
+
+    data_sku = pd.read_excel(path_sku_time_map, sheet_name='Part 1', usecols='B,C')
+    data_sku.dropna(axis=0, how='any', inplace=True)
+    data_sku.reset_index(inplace=True, drop=True)
+    data_sku=data_sku.astype('int')
+    print(data_sku)
+
+    data_order = pd.read_excel(path_order_sku_map, sheet_name='Part 1', usecols='B,D',
+                         names=['CommodityID','PosGroupID'])
+    ofo=['PosGroupID','CommodityID']
+    data_order=data_order[ofo]
+
+    wnc=list(data_order.groupby(['PosGroupID','CommodityID']).groups.keys())
+    print(len(wnc))
+    wnc_sku=list(data_sku.groupby(['PosGroupID','CommodityID']).groups.keys())
+    # print(wnc_sku)
+    print(len(wnc_sku))
+
+    for i in wnc:
+        if i not in wnc_sku:
+            print(i)
+    # ordersku_array=data_order.values
+    # sku_array=data_sku.values
+    # sku_array=data_sku['CommodityID'].to_list()
+    # 每个工区各40个sku
+
+def combine_data():
+    # 修改新的picklinepos
+    cwd = os.getcwd()  # 读取当前文件夹路径
+
+    # path_order_sku_map = cwd + '/Fa_data/OrderPickDetail.xlsx'
+    path_sku_time_map = cwd + '/Fa_data/basedata/PickLinePos_time.xlsx'
+    path_adjust=cwd+'/Fa_data/basedata/excel_1.xlsx' # 用来手动调整时间
+    # 初始化sku所在的分区：sku名称，sku处理所需时间、sku所在分区
+    df = pd.read_excel(path_sku_time_map, sheet_name='Part 1', usecols='A,B,C,D')
+    df.dropna(axis=0, how='any', inplace=True)
+    df.reset_index(inplace=True, drop=True)
+    data_count = df.values
+
+    num_sku = len(data_count)
+    df_a = pd.read_excel(path_adjust, sheet_name='Sheet1', usecols='A,B,D')
+    print('所有Sku数量为：%d ' % num_sku)
+    print(df)
+    print(df_a)
+
+    dfm = pd.merge(df, df_a, on=['CommodityID', 'PosGroupID'], how='left')
+    # dfm=dfm.drop('Time',axis=1)
+
+    # data_skuorder.dropna(axis=0, how='any', inplace=True)
+    # print(data_skuorder[data_skuorder.isna().any(axis=1)])
+    print(dfm)
+    print(len(dfm))
+    dfm.to_excel(cwd+'/Fa_data/PickLinePos_time.xlsx',sheet_name='Part 1',index=False)
+
+
+if __name__ == "__main__":
+    vars=[1]*240 #list(np.ones(235))
+    # flag=1：重新赋值，0：取表
+    # test_sku_section_popularity()
+    # test_sku_amount_popularity()
+    # sku_all_used()
+    combine_data()
+    busy_sad=evaluation(vars,flag=0)
---
+++ Storage/Dynamic++_GA_main_multiprocess.py
@@ -0,0 +1,160 @@
+import time as tm
+import geatpy as ea
+import numpy as np
+
+from a_Simulation import Simulation
+from a_Dynamic_simulation_config import simulation_config
+import multiprocessing as mp
+
+
+class GaSolver(ea.Problem):
+
+    def __init__(self, simulation_config):
+        self.iteration_counter = 0
+        name = 'GA_Simulation'
+        M = 1  # 目标维数
+        maxormins = [1]  # 目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标
+        Dim = 9  # 决策变量维数 a,b,c共3个参数
+        varTypes = [0] * Dim  # 决策变量的类型列表，0：实数；1：整数
+        lb = [0] * Dim  # 决策变量下界
+        ub = [1] * Dim  # 决策变量上界
+        lbin = [1] * Dim  # 决策变量下边界（0表示不包含该变量的下边界，1表示包含）
+        ubin = [1] * Dim  # 决策变量上边界（0表示不包含该变量的上边界，1表示包含）
+        # 调用父类构造方法完成实例化
+        ea.Problem.__init__(self, name, M, maxormins, Dim, varTypes, lb, ub, lbin, ubin)
+        self.simulation_1=Simulation(simulation_config)
+
+    def obj_for_parallel(self,x_id,x):
+        results=[]
+
+        xx=trans_x(x)
+        print(xx)
+        self.simulation_1.recycle_initial()  # 第二次仿真需要重启部分设置
+        results_sim = self.simulation_1.run(rule=xx)  # 运行仿真
+        T_last = results_sim['T_last']
+        busy_variance = results_sim['busy_variance']
+        all = results_sim['all']
+
+        if simulation_config['type'] == 'min_timespan':
+            result_x = float(T_last)
+        elif simulation_config['type'] == 'min_variance':
+            result_x= float(busy_variance)
+        elif simulation_config['type'] == 'min_all':
+            result_x = float(all)
+        else:
+            print('未定义返回参数！')
+            result_x= float(T_last)
+
+        results.append([x_id,result_x])
+        print(results)
+        return results
+
+    def evalVars(self, Vars):
+        # 评估目标函数
+        ObjV=[]
+        # total_cost_array = np.zeros(Vars.shape[0])
+        num_cores = 1  # 并行核数
+        print('num_cores:',num_cores)
+        param_list = []  # 保存并行参数
+        for id in range(Vars.shape[0]):
+            param_list.append([id,Vars.tolist()[id]])
+
+        pool = mp.Pool(num_cores)
+        result = [pool.apply_async(self.obj_for_parallel, args=(item[0], item[1])) for item in param_list]
+        result = [p.get() for p in result]  # result is a list of list
+        flatten_result = []
+        for item in result:
+            flatten_result.extend(item)
+        flatten_result.sort(key=lambda x: x[0])  # 根据id顺序排序(升序)
+        for i in range(Vars.shape[0]):
+            ObjV.append([flatten_result[i][1]])
+        ObjV = np.array(ObjV)
+        return ObjV  # , CV  返回目标函数值矩阵和违反约束程度矩阵
+
+        # return total_cost_array.reshape(-1, 1)  # 固定为1列，多少行不知道
+
+def trans_x(xx):
+    count=0
+    list_inner=[]
+    list_outer=[]
+    for i in xx:
+        list_inner.append(i)
+        count=count+1
+        if(count==3):
+            list_outer.append(list_inner)
+            count=0
+            list_inner=[]
+    # print(len(list_outer),len(list_outer[0]))
+    return list_outer
+
+def example_GA(simulation_config):
+    # 实例化问题对象
+    object_value_list = []
+    solution_list = []
+
+    seed_list = range(0, simulation_config['seed_num'])
+
+    # test_solution = Simulation(simulation_config)
+
+    problem = GaSolver(simulation_config)
+    # 实例化种群对象:Encoding编码方式,NIND所需要的个体数,RI-实整数编码，实数和整数的混合编码
+    population = ea.Population(Encoding='RI', NIND=simulation_config['NIND'])
+    # 试图加入初始解[未成功]
+    # chrome=np.zeros((simulation_config['NIND'],3))
+    # chrome=np.array([[1,0,0],[1,0.5,0.3]])
+    # tempPop.initChrom()  # 初始化种群染色体矩阵
+    # population.Phen=chrome
+    # print(population.__str__())
+
+    # 构建算法：增强精英保留的遗传算法模板
+    algorithm = ea.soea_SEGA_templet(problem=problem, population=population,
+                                     # ea.Population(Encoding='RI', NIND=simulation_config['NIND']), # 10+
+                                     # 实例化种群对象:Encoding编码方式,NIND所需要的个体数,RI-实整数编码，实数和整数的混合编码
+                                     MAXGEN=simulation_config['MaxGen'],  # 最大进化代数
+                                     MAXTIME=simulation_config['MaxTime'],  # 最大运行时间
+                                     logTras=1,  # 表示每隔多少代记录一次日志信息，0表示不记录。
+                                     trappedValue=1e-6,  # 单目标优化陷入停滞的判断阈值。
+                                     maxTrappedCount=15)  # 进化停滞计数器最大上限值。
+    algorithm.mutOper = ea.Mutbga(Pm=0.25, MutShrink=0.5, Gradient=20)  # 变异率设定
+
+    for seed in seed_list:
+        # 求解
+        print("\n当前seed:{}".format(seed))
+        res = ea.optimize(algorithm,
+                          seed=seed,  # int - 随机数种子
+                          verbose=True,  # bool - 控制是否在输入输出流中打印输出日志信息。
+                          drawing=0,  # int  - 算法类控制绘图方式的参数,0表示不绘图；
+                          outputMsg=True,  # bool - 控制是否输出结果以及相关指标信息。
+                          drawLog=False,  # bool - 用于控制是否根据日志绘制迭代变化图像。
+                          saveFlag=False,  # bool - 控制是否保存结果。
+                          dirName=None  # str  - 文件保存的路径。当缺省或为None时，默认保存在当前工作目录的文件夹下
+                          )
+
+        # 评估最优解并输出图像
+        test_solution = Simulation(simulation_config)
+        answer_list=trans_x(res['Vars'][0])
+        print(answer_list)
+        print(len(answer_list),len(answer_list[0]))
+        results = test_solution.run(rule=answer_list)  # 运行仿真
+        solution_list.append(res['Vars'][0])
+        object_value_list.append(results['T_last'])
+
+    # 多个seed迭代结束后，取最小的object value
+    # print(object_value_list)
+    min = object_value_list.index(np.min(object_value_list))  # 总成本最小方案
+    print('\n\n当前object_value_list:{},取第{}个结果'.format(object_value_list, min))
+    return object_value_list,solution_list,min
+
+if __name__ == '__main__':
+    start = tm.time()
+    object_value_list, solution_list, min=example_GA(simulation_config)
+
+
+    best_object = object_value_list[min]
+    best_solution=solution_list[min]
+
+    print('当前min的目标是:',simulation_config['type'],simulation_config['rule'])
+    print(f'最优解为:{list(best_solution)},目标值为:{best_object}')
+
+    end = tm.perf_counter()
+    print("程序共计用时 : %s Seconds " % (end - start))
